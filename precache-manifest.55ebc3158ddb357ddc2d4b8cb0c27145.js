6b6f2e218cf7b14a5710773abb601619f64900c6
Juan Sebastian Delgado Rincon <juansebastiandelgadorincon@Juans-MacBook-Pro.local>
2019-07-23 21:49:51 -0500

    Updates

---
+++ precache-manifest.55ebc3158ddb357ddc2d4b8cb0c27145.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2815c052e51b6b95c37ea85f8b2363c1",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "ba79c6d05fc25ec6979c",
+    "url": "/clickyGame/static/css/main.3b04ef45.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ba79c6d05fc25ec6979c",
+    "url": "/clickyGame/static/js/main.af6bb0da.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
